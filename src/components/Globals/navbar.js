50cf069d95257375982cffeeb067c754486d8e59
Prashanith <prashanith007@gmail.com>
2021-10-06 16:15:22 +0530

    Init

---
+++ src/components/Globals/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div style={{background:'#071A2F'}}>
+        
+            
+            
+        </div>
+    )
+}
+
+export default Navbar
