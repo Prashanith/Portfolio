334c2a538b01f11eed1440fcfc5adfcff4630440
Prashanith <prashanith007@gmail.com>
2025-11-01 09:31:55 +0530

    animate

---
+++ src/components/animate/animate.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import { IClassName } from "../../utils/IClassName";
+import { motion } from "framer-motion";
+
+interface IAnimate extends IClassName {
+  children: ReactNode;
+}
+function Animate({ className = "", children }: IAnimate) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 3 }}
+      viewport={{ once: false }}
+      className={className}
+    >
+      {children && children}
+    </motion.div>
+  );
+}
+
+export default Animate;
--- src/features/landingpage/about/about.tsx
+++ src/features/landingpage/about/about.tsx
@@ -1,27 +1,26 @@
-import { motion } from "framer-motion";
+import Animate from "../../../components/animate/animate";
 
 function About() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 3 }}
-      viewport={{ once: false }}
-      id="about"
-      className="pagePadding mx-auto flex flex-col  items-start justify-center py-16 text-left sm:py-28 md:items-center md:text-center"
-    >
-      <p className="boldText">About Me</p>
-      <div className="mt-5 self-center text-base">
-        <p className="text-xl font-black italic">
-          Greetings fellow humans! I am Prashanith, a coding sorcerer who
-          dabbles in the mystical arts of full stack development. With my
-          wizardly skills, I freelance clients for web/mobile-app solutions.
-          Making the User Experience magisterial wherever I work is my passion.
-          Improving my wizardry and exploring several things right now.
-          <br />I am LINUX User, I use ARCH, BTW
-        </p>
+    <Animate className=''>
+      <div
+        id='about'
+        className='pagePadding mx-auto flex flex-col  items-start justify-center py-16 text-left sm:py-28 md:items-center md:text-center'
+      >
+        <p className='boldText'>About Me</p>
+        <div className='mt-5 self-center text-base'>
+          <p className='text-xl font-black italic'>
+            Greetings fellow humans! I am Prashanith, a coding sorcerer who
+            dabbles in the mystical arts of full stack development. With my
+            wizardly skills, I freelance clients for web/mobile-app solutions.
+            Making the User Experience magisterial wherever I work is my
+            passion. Improving my wizardry and exploring several things right
+            now.
+            <br />I am LINUX User, I use ARCH, BTW
+          </p>
+        </div>
       </div>
-    </motion.div>
+    </Animate>
   );
 }
 
